3690ce73db9cb43ba8f3827eccba91ca0aac748a
javad <javad.feyzabadi@yahoo.com>
2023-02-26 18:25:49 +0330

    crate model

--- home/views.py
+++ home/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.views import View
 
 class Home(View):
-    http_method_names=['get','options']
+    # http_method_names=['get','options']
 
     def get(self,request):
         return render(request,'home/home.html')
 
-    def options(self, request, *args, **kwargs):
-        response = super().options(request, *args, **kwargs)
-        response.headers['host']='localhost'
-        response.headers['user']=request.user
-        return response
+    # def options(self, request, *args, **kwargs):
+    #     response = super().options(request, *args, **kwargs)
+    #     response.headers['host']='localhost'
+    #     response.headers['user']=request.user
+    #     return response
 
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        return render(request,'method_not_allowed.html') 
+    # def http_method_not_allowed(self, request, *args, **kwargs):
+    #     return render(request,'method_not_allowed.html') 
