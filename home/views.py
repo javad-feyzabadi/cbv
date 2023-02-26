a1da47d8ec9edfac0bf686b32c0143108066b05a
javad <javad.feyzabadi@yahoo.com>
2023-02-26 18:40:38 +0330

    templateview

--- home/views.py
+++ home/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
+from . models import Car
 
-class Home(View):
+
+class Home(TemplateView):
+    template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cars'] = Car.objects.all()
+        return context
+
     # http_method_names=['get','options']
 
-    def get(self,request):
-        return render(request,'home/home.html')
+    # def get(self,request):
+    #     return render(request,'home/home.html')
 
     # def options(self, request, *args, **kwargs):
     #     response = super().options(request, *args, **kwargs)
