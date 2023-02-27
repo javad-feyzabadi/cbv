cd4ba8a4bc12fd83593b94eca5daf30eaf3b4e96
javad <javad.feyzabadi@yahoo.com>
2023-02-27 21:08:06 +0330

    edit model

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ class Car(models.Model):
     name = models.CharField(max_length=100)
     owner = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
         return self.name
